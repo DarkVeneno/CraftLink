f46805c72580906d133a089d3669f0e89a1ce9b4
DarkVeneno <carlosrodriguesalmeida@gmail.com>
2024-07-06 21:18:03 +0100

    Update scripts.js

--- web_resources/scripts.js
+++ web_resources/scripts.js
@@ -2,5 +2,5 @@ function performSearch() {
     const query = document.getElementById('search-input').value;
     const engine = document.getElementById('search-engine').value;
     const url = engine + encodeURIComponent(query);
-    window.open(url, '_blank');
+    window.open(url, '_self');
 }
